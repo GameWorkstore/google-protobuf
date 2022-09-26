8bfebdb2653659c8dbc9215b26663a33f56aa27a
John Willy de Souza Rosa <woodimbr@gmail.com>
2022-09-26 12:34:09 +0200

    Update ProtobufConfig.cs

--- ProtobufUnitySDK/Assets/Package/Runtime/Google.Protobuf.Config/ProtobufConfig.cs
+++ ProtobufUnitySDK/Assets/Package/Runtime/Google.Protobuf.Config/ProtobufConfig.cs
@@ -5,10 +5,10 @@ namespace Google.Protobuf.Config
     [CreateAssetMenu(fileName = "ProtobufConfig", menuName = "Protobuf/ProtobufConfig")]
     public class ProtobufConfig : ScriptableObject
     {
-        public bool ProtocolCompilerEnabled = false;
+        public bool ProtocolCompilerEnabled = true;
 
         [Header("C#")]
-        public bool CSharpCompilerEnabled = false;
+        public bool CSharpCompilerEnabled = true;
         [Tooltip("Leave custom paths empty if it should compile right next to original.")]
         public string CSharpCustomPath = string.Empty;
 
